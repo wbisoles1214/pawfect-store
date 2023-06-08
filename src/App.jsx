7ed009893cf0be9f58de3cd4fe2eb11f473d4ac3
wbisoles1214 <wbisoles@gmail.com>
2023-06-08 18:48:08 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">PawFect Store</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
